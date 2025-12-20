16b50cf6667afb4664e8bb2ecb2aa699c1670b9f
Alexander <ytsasha382@mail.ru>
2025-12-20 10:27:27 +0500

    redis

--- internal/services/gamestatsService/get_leaderboard.go
+++ internal/services/gamestatsService/get_leaderboard.go
@@ -2,32 +2,32 @@ package gamestatsService
 
 import (
 	"context"
-	//"encoding/json"
-	//"time"
+	"encoding/json"
+	"time"
 
 	"github.com/JustRussianGuy/GameStats/internal/models"
-	//"github.com/JustRussianGuy/GameStats/internal/redis"
+	"github.com/JustRussianGuy/GameStats/internal/redis"
 )
 
 func (s *GameStatsService) GetLeaderboard(ctx context.Context, limit int) ([]*models.PlayerStats, error) {
 	// Пытаемся получить из Redis
-	/*
+	
 	cached, err := redis.RDB.Get(ctx, "leaderboard").Result()
 	if err == nil {
 		var stats []*models.PlayerStats
 		json.Unmarshal([]byte(cached), &stats)
 		return stats, nil
 	}
-	*/
+	
 	// Если нет в кэше, берём из storage
 	stats, err := s.storage.GetLeaderboard(ctx, limit)
 	if err != nil {
 		return nil, err
 	}
-	/*
+	
 	// Сохраняем в Redis
 	data, _ := json.Marshal(stats)
 	redis.RDB.Set(ctx, "leaderboard", data, 5*time.Second)
-	*/
+	
 	return stats, nil
 }
